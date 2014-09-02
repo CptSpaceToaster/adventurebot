84812addda546d9aa5beca8a7d43a571835255af
CptSpaceToaster <Lugia978@yahoo.com>
2014-09-02 14:45:59 -0400

    Some example rooms

--- robots/adef.go
+++ robots/adef.go
@@ -8,16 +8,16 @@ type Player struct {
 }
 
 type Room struct {
-    Name            string          `json:"name"`
+    Names           []string        `json:"names"`
     ID              string          `json:"id"`
     Description     string          `json:"description"`
     Parent          string          `json:"parent,omitempty"`
-    
+
     Has_Ceiling     bool            `json:"has_ceiling"`
     Has_Floor       bool            `json:"has_floor"`
     Is_Outside      bool            `json:"is_outside"`
-    
-    Adjacent        []string        `json:"adjacent"`
+
+    Adjacent        []string        `json:"adjacent,omitempty"`
     North           string          `json:"north,omitempty"`
     North_East      string          `json:"north_east,omitempty"`
     East            string          `json:"east,omitempty"`
@@ -28,19 +28,35 @@ type Room struct {
     North_West      string          `json:"north_west,omitempty"`
     Up              string          `json:"up,omitempty"`
     Down            string          `json:"down,omitempty"`
-    
+
     Items           []string        `json:"items,omitempty"`
     Widgets         []string        `json:"widgets,omitempty"`
 }
 
 type Item struct {
-    Name            string          `json:"name"`
+    Names           []string        `json:"name"`
+    ID              string          `json:"id
     Description     string          `json:"description"`
     Amount          int             `json:"amount,omitempty"`
+    Actions         []Action        `json:"actions,omitempty"`
 }
 
 type Widget struct {
-    Name            string          `json:"name"`
+    Names           []string        `json:"name"`
+    ID              string          `json:"id"`
     Description     string          `json:"description"`
-    Actions         []string        `json:"actions"`
+    Actions         []Action        `json:"actions,omitempty"`
+}
+
+type Action struct {
+    Commands        []string        `json:"commands"`
+    Result          string          `json:"result"`
+    Teleport        string          `json:"teleport,omitempty"`
+    Requires        []Requirement   `json:"requires,omitempty"`
+    Gives           []string        `json:"gives,omitempty"`
+}
+
+type Requirement struct {
+    Name            string          `json:"name"`
+    Consumed        bool            `json:"consumed"`
 }
